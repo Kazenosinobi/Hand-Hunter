987b6c38a7b0c7b29dacda1b4a8d2486e0652536
KillReal34 <85308586+KillReal34@users.noreply.github.com>
2025-01-28 23:15:29 +0300

    HH-34: Удалить класс DatabaseError

--- app/src/main/java/ru/practicum/android/diploma/features/common/domain/CustomException.kt
+++ app/src/main/java/ru/practicum/android/diploma/features/common/domain/CustomException.kt
@@ -4,6 +4,5 @@ sealed class CustomException : Exception() {
     data object EmptyError : CustomException()
     data object NetworkError : CustomException()
     data object ServerError : CustomException()
-    data class DatabaseError(val error: String) : CustomException()
     data class RequestError(val code: Int) : CustomException()
 }
